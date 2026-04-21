8d761d281cc0b278f71d1451b059e93000ae7008
pluto <estebansnz@Estebans-MacBook-Air.local>
2026-04-20 21:36:22 -0600

    Fix scrolling

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,10 +2,12 @@ import { Outlet } from 'react-router-dom'
 import Footer from './Footer'
 import GlowBackground from './GlowBackground'
 import Header from './Header'
+import ScrollToTop from './ScrollToTop'
 
 function Layout() {
   return (
     <div className="app-shell">
+      <ScrollToTop />
       <GlowBackground />
       <Header />
       <main>
